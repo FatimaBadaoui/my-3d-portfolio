a43c732141834f77515b3aa39e762d965ff48fb4
Fatima Badaoui <fatimina615@gmail.com>
2024-05-17 09:58:02 +0200

    add get request to root endpoint

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Email Sender API");
+});
+
 app.post("/", emailValidation(), validate, async (req, res) => {
   try {
     const { name, email, message } = req.body;
